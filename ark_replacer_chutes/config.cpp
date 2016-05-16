19e4ab1348d2104160eff8e1b385f6cb23761111
Cyruz143 <root.cyruz@gmail.com>
2016-05-16 11:27:50 +0100

    Required addons setup

---
+++ ark_replacer_chutes/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class ARK_Replacer_Chutes
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Air_F","A3_Air_F_Beta"};
+	};
+};
+class CfgVehicles
+{
+	class Steerable_Parachute_F
+	class ARK_Steerable_Parachute_F: Steerable_Parachute_F
+	{
+
+	};
+};
