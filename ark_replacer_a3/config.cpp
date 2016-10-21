52a95a305cb014e853705578ac6454eb09e28e36
Cyruz143 <root.cyruz@gmail.com>
2016-10-21 14:15:54 +0100

    Tidying up tabs and fixing minor issues

--- ark_replacer_a3/config.cpp
+++ ark_replacer_a3/config.cpp
@@ -1,12 +1,12 @@
 class CfgPatches
 {
-	class ARK_Replacer_A3
-	{
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {"A3_Characters_F","CUP_Weapons_M136"};
-	};
+    class ARK_Replacer_A3
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.64;
+        requiredAddons[] = {"A3_Characters_F","CUP_Weapons_M136"};
+    };
 };
 
 #include "CfgWeapons.hpp"
