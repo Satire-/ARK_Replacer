d3175b62c6b27ced702d3461e7a2c1d6f55e3da7
Cyruz143 <root.cyruz@gmail.com>
2016-10-22 12:14:24 +0100

    Adding missing $ sign

--- ark_replacer_a3/cfgWeapons.hpp
+++ ark_replacer_a3/cfgWeapons.hpp
@@ -8,43 +8,43 @@ class CfgWeapons {
     
     class arifle_SPAR_01_base_F;
     class arifle_SPAR_01_blk_F: arifle_SPAR_01_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_blk;
+        displayName = $STR_ARK_arifle_SPAR_01_blk;
     };
     class arifle_SPAR_01_khk_F: arifle_SPAR_01_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_khk;
+        displayName = $STR_ARK_arifle_SPAR_01_khk;
     };
     class arifle_SPAR_01_snd_F: arifle_SPAR_01_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_snd;
+        displayName = $STR_ARK_arifle_SPAR_01_snd;
     };
     class arifle_SPAR_01_GL_base_F;
     class arifle_SPAR_01_GL_blk_F: arifle_SPAR_01_GL_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_GL_blk;
+        displayName = $STR_ARK_arifle_SPAR_01_GL_blk;
     };
     class arifle_SPAR_01_GL_khk_F: arifle_SPAR_01_GL_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_GL_khk);
+        displayName = $STR_ARK_arifle_SPAR_01_GL_khk;
     };
     class arifle_SPAR_01_GL_snd_F: arifle_SPAR_01_GL_base_F {
-        displayName = STR_ARK_arifle_SPAR_01_GL_snd);
+        displayName = $STR_ARK_arifle_SPAR_01_GL_snd;
     };
     class arifle_SPAR_02_base_F;
     class arifle_SPAR_02_blk_F: arifle_SPAR_02_base_F {
-        displayName = STR_ARK_arifle_SPAR_02_blk);
+        displayName = $STR_ARK_arifle_SPAR_02_blk;
     };
     class arifle_SPAR_02_khk_F: arifle_SPAR_02_base_F {
-        displayName = STR_ARK_arifle_SPAR_02_khk);
+        displayName = $STR_ARK_arifle_SPAR_02_khk;
     };
     class arifle_SPAR_02_snd_F: arifle_SPAR_02_base_F {
-        displayName = STR_ARK_arifle_SPAR_02_snd);
+        displayName = $STR_ARK_arifle_SPAR_02_snd;
     };
     class arifle_SPAR_03_base_F;
     class arifle_SPAR_03_blk_F: arifle_SPAR_03_base_F {
-        displayName = STR_ARK_arifle_SPAR_03_blk);
+        displayName = $STR_ARK_arifle_SPAR_03_blk;
     };
     class arifle_SPAR_03_khk_F: arifle_SPAR_03_base_F {
-        displayName = STR_ARK_arifle_SPAR_03_khk);
+        displayName = $STR_ARK_arifle_SPAR_03_khk;
     };
     class arifle_SPAR_03_snd_F: arifle_SPAR_03_base_F {
-        displayName = STR_ARK_arifle_SPAR_03_snd);
+        displayName = $STR_ARK_arifle_SPAR_03_snd;
     };
     
     class U_I_C_Soldier_Bandit_1_F;
