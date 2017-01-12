8ac4706170245ac4b402e7a962cc2587cabbf567
Cyruz143 <root.cyruz@gmail.com>
2017-01-12 12:49:17 +0000

    Removing accidental space

--- ark_replacer_a3/config.cpp
+++ ark_replacer_a3/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
         units[] = {};
         weapons[] = {};
         requiredVersion = 1.64;
-        requiredAddons[] = {"A3_Characters_F","CUP_Weapons_M136"," A3_Weapons_F_Exp_Rifles_SPAR_01"};
+        requiredAddons[] = {"A3_Characters_F","CUP_Weapons_M136","A3_Weapons_F_Exp_Rifles_SPAR_01"};
     };
 };
 
